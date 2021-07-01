d9c571e69d1fb11ef7997d866e35b71afad668e8
steryn <steryn@qq.com>
2021-07-01 11:09:20 +0800

    no message

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,15 +1,21 @@
 module.exports = {
   title: '个人博客', //标题
-  keywords: '前端开发',
   description: '前端开发 个人博客',
+  head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
+  serviceWorker: true,
   repo: 'https://github.com/steryn/blog.git', //仓库地址
   base: '/eBlog/', // 配合部署项目
-  head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
+  markdown: {
+    lineNumbers: true, // 代码块显示行号
+  },
   lastUpdated: 'Last Updated',
-  base:'/eBlog',
+  displayAllHeaders: true,
   themeConfig: {
     //主题配置
-    logo: '/img/logo.jpg',
+    logo: '/logo.jpg',
+    sidebar: 'auto',
+    sidebarDepth: 2, // e'b将同时提取markdown中h2 和 h3 标题，显示在侧边栏上。
+    lastUpdated: 'Last Updated', // 文档更新时间：每个文件git最后提交的时间
     nav: [
       //导航栏
       { text: '首页', link: '/' },
@@ -30,10 +36,60 @@ module.exports = {
     ],
     sidebar: {
       //侧边拦
-      '/2020/': [
-        ['/2020/5/', '5月份'],
-        ['/2020/6/', '6月份'],
-        ['/2020/7/', '7月份'],
+      // '/2020/': [
+      //   ['/2020/5/', '5月份'],
+      //   ['/2020/6/', '6月份'],
+      //   ['/2020/7/', '7月份'],
+      // ],
+      '/index/': [
+        // 侧边栏在 /index/ 目录上
+        '/index/',
+        {
+          title: 'Html',
+          collapsable: false, // 不可折叠
+          children: ['/html/one.md'],
+        },
+        {
+          title: 'CSS',
+          collapsable: false, // 不可折叠
+          children: ['/index/css/one.md'],
+        },
+        // 侧边栏在 /javascript/ 目录上
+        {
+          title: 'Javascript',
+          collapsable: true, // 不可折叠
+          children: ['/index/javascript/javascript.md'],
+        },
+        // 侧边栏在 /node.js/ 目录上
+        {
+          title: 'nodejs',
+          collapsable: true, // 不可折叠
+          children: ['/index/nodejs/nodejs.md'],
+        },
+        // 侧边栏在 /react.js/ 目录上
+        {
+          title: 'react',
+          collapsable: true, // 不可折叠
+          children: ['/index/nodejs/react.md'],
+        },
+        // 侧边栏在 /others/ 目录上
+        {
+          title: '其它',
+          collapsable: true, // 不可折叠
+          children: ['/index/others/others.md'],
+        },
+        // 侧边栏在 /vue.js/ 目录上
+        {
+          title: 'vuejs',
+          collapsable: true, // 不可折叠
+          children: ['/index/vue/vue.md'],
+        },
+        // 侧边栏在 /vx/ 目录上
+        {
+          title: 'vx',
+          collapsable: true, // 不可折叠
+          children: ['/index/vx/vx.md'],
+        },
       ],
     },
   },
